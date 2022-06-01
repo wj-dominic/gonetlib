5d7b1f013a5c955960b5d8ab1f77cb1fa3a24d0b
dogSyeon <smkk512@gmail.com>
2022-06-02 00:17:11 +0900

    제너레이팅 구현 중

--- generator/generator.go
+++ generator/generator.go
@@ -1,29 +1,28 @@
 package generator
 
 import (
-	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
+	"gonetlib/netlogger"
+	"os"
+	"path"
 )
 
 type Generator struct {
-	IdlMap map[string]map[string]ast.StructType
 }
 
 func NewGenerator() *Generator {
-	return &Generator{
-		IdlMap: make(map[string]map[string]ast.StructType),
-	}
+	return &Generator{}
 }
 
 func (g *Generator) Generate(path string) bool {
 	//.go 파일 파싱
-	if g.parse(path) == false {
+	idlParser := NewIDLParser()
+
+	if idlParser.Parse(path) == false {
 		return false
 	}
 
 	//id 생성
+	g.GenerateID(idlParser)
 
 	//protocol 세팅 인터페이스 생성
 
@@ -32,73 +31,17 @@ func (g *Generator) Generate(path string) bool {
 	return true
 }
 
-func (g *Generator) parse(path string) bool {
-	fset := token.NewFileSet()
-	if fset == nil {
-		fmt.Println("Failed to make a new file set from generator")
-		return false
-	}
+func (g *Generator) GenerateID(parser *IDLParser) bool {
+	for pkgName, structure := range parser.IdlMap {
+		pkgPath := path.Join(".", pkgName)
+		err := os.Mkdir(pkgPath, os.ModeDir)
+		if err != nil && os.IsExist(err) == false {
+			netlogger.GetLogger().Error("Failed to mkdir for generate id | path[%s]", pkgPath)
+			return false
+		}
 
-	pkgs, err := parser.ParseDir(fset, path, nil, parser.ParseComments)
-	if err != nil {
-		fmt.Printf("Failed to parse dir | path[%s] err[%s]\n", path, err)
-		return false
-	}
+		for structureName, _ := range structure {
 
-	for _, pkg := range pkgs {
-		for _, file := range pkg.Files {
-			for _, node := range file.Decls {
-				switch node.(type) {
-				case *ast.GenDecl:
-					genDecl := node.(*ast.GenDecl)
-					for _, spec := range genDecl.Specs {
-						switch spec.(type) {
-						case *ast.TypeSpec:
-							typeSpec := spec.(*ast.TypeSpec)
-							switch typeSpec.Type.(type) {
-							case *ast.StructType:
-								if _, exist := g.IdlMap[file.Name.Name]; exist == false {
-									g.IdlMap[file.Name.Name] = make(map[string]ast.StructType)
-								}
-
-								structType := typeSpec.Type.(*ast.StructType)
-								if _, exist := g.IdlMap[file.Name.Name][typeSpec.Name.Name]; exist == true {
-									continue
-								}
-
-								g.IdlMap[file.Name.Name][typeSpec.Name.Name] = *structType
-
-								//g.IdlMap[typeSpec.Name.Name] = *structType
-								// for _, field := range structType.Fields.List {
-								// 	var fieldType string
-
-								// 	switch field.Type.(type) {
-								// 	case *ast.ArrayType:
-								// 		fieldArray := field.Type.(*ast.ArrayType)
-								// 		fieldType = fieldArray.Elt.(*ast.Ident).Name
-								// 		break
-
-								// 	case *ast.Ident:
-								// 		fieldIdent := field.Type.(*ast.Ident)
-								// 		fieldType = fieldIdent.Name
-								// 		break
-
-								// 	case *ast.MapType:
-								// 		fieldMap := field.Type.(*ast.MapType)
-								// 		fieldType = fieldMap.Key.(*ast.Ident).Name
-								// 	}
-
-								// 	for _, name := range field.Names {
-								// 		fmt.Printf("\tField: name=%s type=%s\n", name.Name, fieldType)
-								// 	}
-
-								// }
-
-							}
-						}
-					}
-				}
-			}
 		}
 	}
 
--- generator/parser.go
+++ generator/parser.go
@@ -1,4 +1,83 @@
 package generator
 
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"go/types"
+)
+
+type Field struct {
+	Name string
+	Type string
+}
+
 type IDLParser struct {
+	IdlMap map[string]map[string][]Field
+}
+
+func NewIDLParser() *IDLParser {
+	return &IDLParser{
+		IdlMap: make(map[string]map[string][]Field),
+	}
+}
+
+func (p *IDLParser) Parse(path string) bool {
+	if len(path) <= 0 {
+		return false
+	}
+
+	fset := token.NewFileSet()
+	if fset == nil {
+		fmt.Println("Failed to make a new file set from generator")
+		return false
+	}
+
+	pkgs, err := parser.ParseDir(fset, path, nil, parser.ParseComments)
+	if err != nil {
+		fmt.Printf("Failed to parse dir | path[%s] err[%s]\n", path, err)
+		return false
+	}
+
+	for _, pkg := range pkgs {
+		for _, file := range pkg.Files {
+			for _, node := range file.Decls {
+				switch node.(type) {
+				case *ast.GenDecl:
+					genDecl := node.(*ast.GenDecl)
+					for _, spec := range genDecl.Specs {
+						switch spec.(type) {
+						case *ast.TypeSpec:
+							typeSpec := spec.(*ast.TypeSpec)
+							switch typeSpec.Type.(type) {
+							case *ast.StructType:
+								if _, exist := p.IdlMap[file.Name.Name]; exist == false {
+									p.IdlMap[file.Name.Name] = make(map[string][]Field)
+								}
+
+								structType := typeSpec.Type.(*ast.StructType)
+								if _, exist := p.IdlMap[file.Name.Name][typeSpec.Name.Name]; exist == true {
+									continue
+								}
+
+								p.IdlMap[file.Name.Name][typeSpec.Name.Name] = make([]Field, len(structType.Fields.List))
+
+								for index, field := range structType.Fields.List {
+									fieldType := field.Type
+									typeString := types.ExprString(fieldType)
+									for _, fieldName := range field.Names {
+										tmpField := Field{Name: fieldName.Name, Type: typeString}
+										p.IdlMap[file.Name.Name][typeSpec.Name.Name][index] = tmpField
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return true
 }
