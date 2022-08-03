86287b6f7d470e4e1db2f80c82a46186977a140e
dogSyeon <smkk512@gmail.com>
2022-08-04 01:00:11 +0900

    protobuf 시리얼라이즈 테스트

--- idl/idl.pb.go
+++ idl/idl.pb.go
@@ -20,16 +20,66 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-type REQ_ECHO struct {
+type ID int32
+
+const (
+	ID_NONE     ID = 0
+	ID_REQ_ECHO ID = 1
+	ID_RES_ECHO ID = 2
+)
+
+// Enum value maps for ID.
+var (
+	ID_name = map[int32]string{
+		0: "NONE",
+		1: "REQ_ECHO",
+		2: "RES_ECHO",
+	}
+	ID_value = map[string]int32{
+		"NONE":     0,
+		"REQ_ECHO": 1,
+		"RES_ECHO": 2,
+	}
+)
+
+func (x ID) Enum() *ID {
+	p := new(ID)
+	*p = x
+	return p
+}
+
+func (x ID) String() string {
+	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+}
+
+func (ID) Descriptor() protoreflect.EnumDescriptor {
+	return file_idl_proto_enumTypes[0].Descriptor()
+}
+
+func (ID) Type() protoreflect.EnumType {
+	return &file_idl_proto_enumTypes[0]
+}
+
+func (x ID) Number() protoreflect.EnumNumber {
+	return protoreflect.EnumNumber(x)
+}
+
+// Deprecated: Use ID.Descriptor instead.
+func (ID) EnumDescriptor() ([]byte, []int) {
+	return file_idl_proto_rawDescGZIP(), []int{0}
+}
+
+type ReqEcho struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Message string `protobuf:"bytes,1,opt,name=message,proto3" json:"message,omitempty"`
+	From    string `protobuf:"bytes,2,opt,name=from,proto3" json:"from,omitempty"`
+	Message string `protobuf:"bytes,3,opt,name=message,proto3" json:"message,omitempty"`
 }
 
-func (x *REQ_ECHO) Reset() {
-	*x = REQ_ECHO{}
+func (x *ReqEcho) Reset() {
+	*x = ReqEcho{}
 	if protoimpl.UnsafeEnabled {
 		mi := &file_idl_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -37,13 +87,13 @@ func (x *REQ_ECHO) Reset() {
 	}
 }
 
-func (x *REQ_ECHO) String() string {
+func (x *ReqEcho) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*REQ_ECHO) ProtoMessage() {}
+func (*ReqEcho) ProtoMessage() {}
 
-func (x *REQ_ECHO) ProtoReflect() protoreflect.Message {
+func (x *ReqEcho) ProtoReflect() protoreflect.Message {
 	mi := &file_idl_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -55,28 +105,36 @@ func (x *REQ_ECHO) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use REQ_ECHO.ProtoReflect.Descriptor instead.
-func (*REQ_ECHO) Descriptor() ([]byte, []int) {
+// Deprecated: Use ReqEcho.ProtoReflect.Descriptor instead.
+func (*ReqEcho) Descriptor() ([]byte, []int) {
 	return file_idl_proto_rawDescGZIP(), []int{0}
 }
 
-func (x *REQ_ECHO) GetMessage() string {
+func (x *ReqEcho) GetFrom() string {
+	if x != nil {
+		return x.From
+	}
+	return ""
+}
+
+func (x *ReqEcho) GetMessage() string {
 	if x != nil {
 		return x.Message
 	}
 	return ""
 }
 
-type RES_ECHO struct {
+type ResEcho struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Message string `protobuf:"bytes,1,opt,name=message,proto3" json:"message,omitempty"`
+	To      string `protobuf:"bytes,1,opt,name=to,proto3" json:"to,omitempty"`
+	Message string `protobuf:"bytes,2,opt,name=message,proto3" json:"message,omitempty"`
 }
 
-func (x *RES_ECHO) Reset() {
-	*x = RES_ECHO{}
+func (x *ResEcho) Reset() {
+	*x = ResEcho{}
 	if protoimpl.UnsafeEnabled {
 		mi := &file_idl_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -84,13 +142,13 @@ func (x *RES_ECHO) Reset() {
 	}
 }
 
-func (x *RES_ECHO) String() string {
+func (x *ResEcho) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*RES_ECHO) ProtoMessage() {}
+func (*ResEcho) ProtoMessage() {}
 
-func (x *RES_ECHO) ProtoReflect() protoreflect.Message {
+func (x *ResEcho) ProtoReflect() protoreflect.Message {
 	mi := &file_idl_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -102,12 +160,19 @@ func (x *RES_ECHO) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use RES_ECHO.ProtoReflect.Descriptor instead.
-func (*RES_ECHO) Descriptor() ([]byte, []int) {
+// Deprecated: Use ResEcho.ProtoReflect.Descriptor instead.
+func (*ResEcho) Descriptor() ([]byte, []int) {
 	return file_idl_proto_rawDescGZIP(), []int{1}
 }
 
-func (x *RES_ECHO) GetMessage() string {
+func (x *ResEcho) GetTo() string {
+	if x != nil {
+		return x.To
+	}
+	return ""
+}
+
+func (x *ResEcho) GetMessage() string {
 	if x != nil {
 		return x.Message
 	}
@@ -117,13 +182,18 @@ func (x *RES_ECHO) GetMessage() string {
 var File_idl_proto protoreflect.FileDescriptor
 
 var file_idl_proto_rawDesc = []byte{
-	0x0a, 0x09, 0x69, 0x64, 0x6c, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x24, 0x0a, 0x08, 0x52,
-	0x45, 0x51, 0x5f, 0x45, 0x43, 0x48, 0x4f, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61,
-	0x67, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67,
-	0x65, 0x22, 0x24, 0x0a, 0x08, 0x52, 0x45, 0x53, 0x5f, 0x45, 0x43, 0x48, 0x4f, 0x12, 0x18, 0x0a,
-	0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07,
-	0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x42, 0x08, 0x5a, 0x06, 0x2e, 0x2e, 0x2f, 0x69, 0x64,
-	0x6c, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x0a, 0x09, 0x69, 0x64, 0x6c, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x37, 0x0a, 0x07, 0x52,
+	0x65, 0x71, 0x45, 0x63, 0x68, 0x6f, 0x12, 0x12, 0x0a, 0x04, 0x66, 0x72, 0x6f, 0x6d, 0x18, 0x02,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x66, 0x72, 0x6f, 0x6d, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65,
+	0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73,
+	0x73, 0x61, 0x67, 0x65, 0x22, 0x33, 0x0a, 0x07, 0x52, 0x65, 0x73, 0x45, 0x63, 0x68, 0x6f, 0x12,
+	0x0e, 0x0a, 0x02, 0x74, 0x6f, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x74, 0x6f, 0x12,
+	0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09,
+	0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x2a, 0x2a, 0x0a, 0x02, 0x49, 0x44, 0x12,
+	0x08, 0x0a, 0x04, 0x4e, 0x4f, 0x4e, 0x45, 0x10, 0x00, 0x12, 0x0c, 0x0a, 0x08, 0x52, 0x45, 0x51,
+	0x5f, 0x45, 0x43, 0x48, 0x4f, 0x10, 0x01, 0x12, 0x0c, 0x0a, 0x08, 0x52, 0x45, 0x53, 0x5f, 0x45,
+	0x43, 0x48, 0x4f, 0x10, 0x02, 0x42, 0x08, 0x5a, 0x06, 0x2e, 0x2e, 0x2f, 0x69, 0x64, 0x6c, 0x62,
+	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -138,10 +208,12 @@ func file_idl_proto_rawDescGZIP() []byte {
 	return file_idl_proto_rawDescData
 }
 
+var file_idl_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
 var file_idl_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_idl_proto_goTypes = []interface{}{
-	(*REQ_ECHO)(nil), // 0: REQ_ECHO
-	(*RES_ECHO)(nil), // 1: RES_ECHO
+	(ID)(0),         // 0: ID
+	(*ReqEcho)(nil), // 1: ReqEcho
+	(*ResEcho)(nil), // 2: ResEcho
 }
 var file_idl_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -158,7 +230,7 @@ func file_idl_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_idl_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*REQ_ECHO); i {
+			switch v := v.(*ReqEcho); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -170,7 +242,7 @@ func file_idl_proto_init() {
 			}
 		}
 		file_idl_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*RES_ECHO); i {
+			switch v := v.(*ResEcho); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -187,13 +259,14 @@ func file_idl_proto_init() {
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_idl_proto_rawDesc,
-			NumEnums:      0,
+			NumEnums:      1,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
 		GoTypes:           file_idl_proto_goTypes,
 		DependencyIndexes: file_idl_proto_depIdxs,
+		EnumInfos:         file_idl_proto_enumTypes,
 		MessageInfos:      file_idl_proto_msgTypes,
 	}.Build()
 	File_idl_proto = out.File
--- idl/idl_test.go
+++ idl/idl_test.go
@@ -1,29 +1,146 @@
 package idl
 
 import (
+	"encoding/binary"
 	"fmt"
+	reflect "reflect"
 	"testing"
 
 	"google.golang.org/protobuf/proto"
 )
 
-func TestIDL(t *testing.T) {
-	reqEcho := &REQ_ECHO{Message: "test"}
+type IPacket[T any] interface {
+	GetID() uint32
+	GetMessage() T
+}
 
-	fmt.Printf("reqEcho: %v\n", reqEcho)
+type ProtobufPacket struct {
+	ID      uint32
+	Message proto.Message
+}
 
-	out, err := proto.Marshal(reqEcho)
-	if err != nil {
-		t.Failed()
+// GetID implements IPacket
+func (p *ProtobufPacket) GetID() uint32 {
+	return p.ID
+}
+
+// GetMessage implements IPacket
+func (p *ProtobufPacket) GetMessage() proto.Message {
+	return p.Message
+}
+
+func NewProtobufPacket(id uint32, message proto.Message) IPacket[proto.Message] {
+	return &ProtobufPacket{
+		ID:      id,
+		Message: message,
+	}
+}
+
+type IPacketFactory[T any] interface {
+	GetPacket(id uint32) T
+}
+
+type ProtobufPacketFactory struct {
+}
+
+// GetPacket implements IPacketFactory
+func (f *ProtobufPacketFactory) GetPacket(id uint32) proto.Message {
+	var msg proto.Message = nil
+
+	switch ID(id) {
+	case ID_REQ_ECHO:
+		msg = new(ReqEcho)
+		break
+	case ID_RES_ECHO:
+		msg = new(ResEcho)
+		break
+	default:
+		break
 	}
 
-	fmt.Printf("out: %v\n", out)
+	return msg
+}
 
-	outReqEcho := &REQ_ECHO{}
-	if proto.Unmarshal(out, outReqEcho) != nil {
+func NewProtobufPacketFactory() IPacketFactory[proto.Message] {
+	return &ProtobufPacketFactory{}
+}
+
+type ISerializer[TInput any, TOutput any] interface {
+	Serialize(data TInput) (TOutput, error)
+	Deserialize(data TOutput) (TInput, error)
+}
+
+type ProtobufSerializer struct {
+	packetFactory IPacketFactory[proto.Message]
+}
+
+func NewProtobufSerializer() ISerializer[IPacket[proto.Message], []byte] {
+	return &ProtobufSerializer{
+		packetFactory: NewProtobufPacketFactory(),
+	}
+}
+
+func (s *ProtobufSerializer) Serialize(data IPacket[proto.Message]) ([]byte, error) {
+	out, err := proto.Marshal(data.GetMessage())
+	if err != nil {
+		return nil, err
+	}
+
+	id := data.GetID()
+	sizeOfId := int(reflect.TypeOf(id).Size())
+
+	buffer := make([]byte, len(out)+sizeOfId)
+	binary.LittleEndian.PutUint32(buffer, id)
+	copy(buffer[sizeOfId:], out)
+
+	return buffer, nil
+}
+
+func (s *ProtobufSerializer) Deserialize(data []byte) (IPacket[proto.Message], error) {
+	sizeOfId := int(reflect.TypeOf((*uint32)(nil)).Elem().Size())
+
+	id := binary.LittleEndian.Uint32(data[0:sizeOfId])
+
+	msg := s.packetFactory.GetPacket(id)
+	if msg == nil {
+		return nil, fmt.Errorf("invalid packet id | id[%d]", id)
+	}
+
+	err := proto.Unmarshal(data[sizeOfId:], msg)
+	if err != nil {
+		return nil, err
+	}
+
+	packet := NewProtobufPacket(id, msg)
+
+	return packet, nil
+}
+
+func TestIDL(t *testing.T) {
+	serializer := NewProtobufSerializer()
+
+	reqEcho := &ReqEcho{From: "1", Message: "test"}
+	fmt.Printf("reqEcho: %v\n", reqEcho)
+
+	packet := NewProtobufPacket(uint32(ID_REQ_ECHO), reqEcho)
+
+	//시리얼라이즈 proto to bytes
+	out, err := serializer.Serialize(packet)
+	if err != nil {
 		t.Failed()
 		return
 	}
 
-	fmt.Printf("outReqEcho: %v\n", outReqEcho)
+	fmt.Printf("out: %v\n", out)
+
+	//디시리얼라이즈 bytes to proto
+	msg, err := serializer.Deserialize(out)
+	if err != nil {
+		t.Failed()
+		return
+	}
+
+	echo := msg.GetMessage().ProtoReflect().Interface().(*ReqEcho)
+
+	fmt.Printf("outReqEcho: %s\n", echo.GetFrom())
 }
